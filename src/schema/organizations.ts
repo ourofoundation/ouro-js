ad9d502a02a38bc1cead5dc1970bb148fe3ea1c1
Matt Moderwell <mmoderwell@gmail.com>
2026-02-12 15:06:44 -0600

    Update org schema

--- src/schema/organizations.ts
+++ src/schema/organizations.ts
@@ -1,18 +1,31 @@
 import {
+  boolean,
   object,
   string,
   array,
   type z,
   optional,
-  nullable
+  nullable,
+  uuid,
 } from "zod";
 
 import { RoleSchema, VisibilitySchema } from "./common";
 import { ProfileSchema } from "./users";
 
+const JoinPolicySchema = string().refine(
+  (val) => ["invite_only", "request", "open"].includes(val),
+  { message: "Invalid join policy" }
+);
+
+const MembershipTypeSchema = string().refine(
+  (val) => ["internal", "external"].includes(val),
+  { message: "Invalid membership type" }
+);
+
 const OrganizationsSchema = object({
-  id: string().uuid(),
+  id: uuid(),
   name: string(),
+  display_name: optional(nullable(string())),
   avatar_path: optional(nullable(string())),
   mission: optional(nullable(string())),
   size: string(),
@@ -28,15 +41,18 @@ const OrganizationsSchema = object({
     )
   ),
   base_role: RoleSchema.default("write"),
+  allow_external_members: optional(boolean()).default(false),
+  join_policy: optional(JoinPolicySchema).default("invite_only"),
 });
 
 const MembershipSchema = object({
-  id: string().uuid(),
-  user_id: string().uuid(),
+  id: uuid(),
+  user_id: uuid(),
   user: optional(nullable(ProfileSchema)),
-  org_id: string().uuid(),
-  team_id: string().uuid(),
+  org_id: uuid(),
+  team_id: uuid(),
   role: RoleSchema,
+  membership_type: optional(MembershipTypeSchema).default("internal"),
   added_at: string().default(() => new Date().toISOString()),
   visibility: VisibilitySchema.default("public"),
 });
